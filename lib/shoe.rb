f361418c3257fa075f8497aba61c2a226737edfb
Kristin Kraemer <kristinhannahk@gmail.com>
2019-05-30 11:39:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,12 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+      BRANDS << brand 
   end
-
-  def brand=(brand)
-    @brand = brand 
-    BRANDS << brand 
-  end 
 
   def cobble
     self.condition = "new"
